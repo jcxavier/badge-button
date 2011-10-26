6c941019cc255940e7bf4806686f5d6590d8feda
João Xavier <ei06116@gmail.com>
2011-10-26 18:27:24 +0100

    first commit - version 1.0, refer to VERSION LOG in the readme

---
+++ src/com/jcxavier/widget/test/BadgeButtonTestActivity.java
@@ -0,0 +1,32 @@
+package com.jcxavier.widget.test;
+
+import com.jcxavier.widget.R;
+import com.jcxavier.widget.BadgeButton;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * @author João Xavier <jcxavier@jcxavier.com>
+ */
+public class BadgeButtonTestActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.badgebtn_test);
+        
+        showButtonWithText(R.id.btn1, "1");
+        showButtonWithText(R.id.btn2, "2");
+        showButtonWithText(R.id.btn3, "3");
+        showButtonWithText(R.id.btn4, "++");
+        showButtonWithText(R.id.btn5, "AM");
+        showButtonWithText(R.id.btn6, "!!");
+    }
+    
+    private void showButtonWithText(int id, String text) {
+        BadgeButton btn = (BadgeButton) findViewById(id);
+        btn.setBadgeText(text);
+        btn.showBadge();
+    }
+}
